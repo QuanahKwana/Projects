ec70506a9eac9fc5508cbaff12bf9524175f9733
QuanahKwana <114540640+QuanahKwana@users.noreply.github.com>
2022-10-01 12:47:38 -0700

    Update depression.py

--- depression.py
+++ depression.py
@@ -1,19 +1,31 @@
 import PySimpleGUI as sg
 
-# I know this doesn't work right now, I'm committing to github to save progress and paste it into my big computer
+# I know this doesn't work right now, I'm committing to gitHub to save progress and paste it into my big computer
 # Sorry if this is a weird thing to code about. I just don't know what else to code rn.
 
 # Layout of the window
-starting_question = [[sg.Text("Hey, I know you've probably seen some stuff.")],
-                     [sg.Text("Would you like to talk about it?")],
+starting_question = [[sg.Text("Hey, I know you've probably seen some stuff.\nWould you like to talk about it?", background_color="lavender", text_color="black")],
                      [sg.Button("Sure", key="-sure-")],
                      [sg.Button("No", key="-no-")],
-                     [sg.Text(size=(69, 1), key='-output-')],
-                     [sg.Button(key='-ok-')]
+                     [sg.Text(size=(69, 1), key='-output-', visible=False, background_color="lavender", text_color="black")],
+                     [sg.Button(key='-ok-', visible=False,)]
                      ]
 
+paragraph = [[sg.Text('Please write down how you feel')],
+             [sg.Multiline(size=(100, 500))]
+             ]
+
+image = [[sg.Image(source="legosi.png")]]
+
+vent = [
+    [
+        sg.Column(paragraph),
+        sg.VerticalSeparator(pad=(50, 50)),
+        sg.Column(image)
+        ]
+]
 # Making Window
-window = sg.Window('Sad App', starting_question)
+window = sg.Window('Sad App', starting_question, background_color="lavender")
 
 # Calling Window
 while True:
@@ -22,10 +34,13 @@ while True:
     if event == sg.WINDOW_CLOSED or event == 'Quit':
         break
     if event == "-sure-":
-        window['-output-'].update("Oh okay! Let's talk about it.")
-        window['-ok-'].update("OK!")
+        window['-output-'].update("Oh okay! Let's talk about it.", visible=True)
+        window['-ok-'].update("OK!", visible=True)
     if event == "-no-":
         break
+    if event == '-ok-':
+        window.close()
+        window = sg.Window('Helping', vent, size=(1920, 1080))
 window.close()
 
 # if talking == "yes" or talking == "sure":
